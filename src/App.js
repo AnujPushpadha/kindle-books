ed80e30a230f705e2d8a774c7eac6add9e7707f4
AnujPushpadha <109943184+AnujPushpadha@users.noreply.github.com>
2023-11-27 21:44:04 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "tailwindcss/tailwind.css"; // Import the tailwind CSS file
 
 function App() {
   const [search, setSearch] = useState("");
@@ -14,26 +15,36 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="container mx-auto p-4">
       <input
         type="text"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
+        className="border rounded p-2 mr-2"
       />
-      <button onClick={handleSearch}>Search</button>
+      <button
+        onClick={handleSearch}
+        className="bg-blue-500 text-white rounded p-2"
+      >
+        Search
+      </button>
 
-      <ul>
+      <ul className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {books.map((book, index) => (
-          <li key={index}>
+          <li key={index} className="border p-4 rounded">
             <img
               src={book.volumeInfo.imageLinks?.thumbnail}
               alt={book.volumeInfo.title}
-              style={{ width: "100px", height: "100px" }}
+              className="w-full h-40 object-cover mb-4"
             />
             <div>
-              <h3>{book.volumeInfo.title}</h3>
-              <p>Author: {book.volumeInfo.authors?.join(", ")}</p>
-              <p>
+              <h3 className="text-lg font-semibold mb-2">
+                {book.volumeInfo.title}
+              </h3>
+              <p className="text-gray-700 mb-2">
+                Author: {book.volumeInfo.authors?.join(", ")}
+              </p>
+              <p className="text-green-500">
                 Price: {book.saleInfo.listPrice?.amount}{" "}
                 {book.saleInfo.listPrice?.currencyCode}
               </p>
